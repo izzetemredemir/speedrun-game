25edff236ba0b74e1f519e2fa746ef15c2eb17a8
İzzet Emre Demir <izzetemredemir@gmail.com>
2023-07-29 23:42:07 +0300

    program add

--- solanaRoyale.rs
+++ solanaRoyale.rs
@@ -1,3 +1,5 @@
+// In src/lib.rs
+
 use solana_program::{
     account_info::AccountInfo, pubkey::Pubkey,
     program_error::ProgramError, entrypoint::ProgramResult,
@@ -7,10 +9,10 @@ use std::collections::HashMap;
 
 solana_program::declare_id!("3sGJfM8VcJt29D1CfrM9tiF7GKvoG3z45P3MGivK2eLc");
 
-use borsh::{BorshSerialize, BorshDeserialize};
 
+use borsh::{BorshSerialize, BorshDeserialize};
 // Define the token struct
-#[derive(Clone, Debug, PartialEq, BorshSerialize, BorshDeserialize)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct Token {
     pub name: String,
     pub symbol: String,
@@ -53,7 +55,7 @@ pub fn create_token(
         decimals,
         owner: *owner_account.key,
         balances: HashMap::new(),
-        owner_names: HashMap::new(),
+        owner_names: HashMap::new(), // Map owner address to their name
     };
 
     // Set the total supply as the balance of the token's owner
@@ -65,6 +67,7 @@ pub fn create_token(
     Ok(())
 }
 
+// Transfer tokens from one address to another
 pub fn transfer(
     accounts: &[AccountInfo],
     amount: u64,
@@ -81,31 +84,24 @@ pub fn transfer(
     }
 
     // Get the token data from the token account
-    let mut data = Token::deserialize(&mut &token_account.data.borrow()[..])?;
+    let mut data = Token::deserialize(&token_account.data.borrow())?;
 
     // Check if the sender has sufficient balance
-    let from_balance_entry = data.balances.get_mut(from_account.key).ok_or(ProgramError::InvalidAccountData)?;
-    if *from_balance_entry < amount {
+    let from_balance = data.balances.get_mut(from_account.key).ok_or(ProgramError::InvalidAccountData)?;
+    if *from_balance < amount {
         return Err(ProgramError::Custom(1)); // InsufficientBalance error (custom error code)
     }
 
-    // Check if the "to" account is different from the "from" account
-    if from_account.key == to_account.key {
-        return Err(ProgramError::InvalidArgument); // Cannot transfer to the same account
-    }
-
     // Perform the transfer
-    //let to_balance_entry = data.balances.get_mut(&to).ok_or(ProgramError::InvalidAccountData)?;
-    *from_balance_entry -= amount;
-    //*to_balance_entry += amount;
+    let to_balance = data.balances.entry(to).or_insert(0);
+    *from_balance -= amount;
+    *to_balance += amount;
 
     // Serialize and store the updated data back to the token account
     data.serialize(&mut &mut token_account.data.borrow_mut()[..])?;
 
     Ok(())
 }
-
-
 
 // Associate a name with the token owner's address
 pub fn set_owner_name(
@@ -122,10 +118,10 @@ pub fn set_owner_name(
     }
 
     // Get the token data from the token account
-    let mut data = Token::deserialize(&mut &token_account.data.borrow()[..])?;
+    let mut data = Token::deserialize(&token_account.data.borrow())?;
 
     // Store the owner's name in the owner_names mapping
-    data.owner_names.insert(*owner_account.key, owner_name);
+    data.owner_names.insert(owner_account.key, owner_name);
 
     // Serialize and store the updated data back to the token account
     data.serialize(&mut &mut token_account.data.borrow_mut()[..])?;
